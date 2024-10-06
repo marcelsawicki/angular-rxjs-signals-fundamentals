6cc9f70b259112f404e042a54ef6acbbf5fb9acf
Marcel <marcel.sawicki@insert.com.pl>
2024-10-06 12:53:26 +0200

    combineLatest

--- apm/src/app/products/product-detail/product-detail.component.ts
+++ apm/src/app/products/product-detail/product-detail.component.ts
@@ -1,50 +1,34 @@
-import { Component, inject, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
-import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
+import { NgIf, NgFor, CurrencyPipe, AsyncPipe } from '@angular/common';
 import { Product } from '../product';
-import { catchError, EMPTY, Subscription } from 'rxjs';
+import { catchError, EMPTY } from 'rxjs';
 import { ProductService } from '../product.service';
 
 @Component({
     selector: 'pm-product-detail',
     templateUrl: './product-detail.component.html',
     standalone: true,
-    imports: [NgIf, NgFor, CurrencyPipe]
+    imports: [AsyncPipe, NgIf, NgFor, CurrencyPipe]
 })
-export class ProductDetailComponent implements OnChanges, OnDestroy{
-  @Input() productId: number = 0;
+export class ProductDetailComponent {
   errorMessage = '';
-  sub!: Subscription;
 
   private productService = inject(ProductService);
 
   // Product to display
-  product: Product | null = null;
-
-  // Set the page title
-  pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
-
-  ngOnChanges(changes: SimpleChanges): void {
-   const id = changes['productId'].currentValue;
-   if (id) {
-    this.sub = this.productService.getProduct(id)
+  product$ = this.productService.product$
     .pipe(
       catchError(err =>{
         this.errorMessage = err;
         return EMPTY;
       })
-    )
-    .subscribe(
-      product => this.product = product
     );
-   }
-  }
 
-  ngOnDestroy(): void {
-    if (this.sub) {
-      this.sub.unsubscribe();
-    }
-  }
+  // Set the page title
+  //pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
+  pageTitle = 'ProductDetail';
+
   addToCart(product: Product) {
   }
 }
