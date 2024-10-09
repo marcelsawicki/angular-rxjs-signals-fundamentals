6681890ba36da5a23210d9527a799ad52a7b1d72
Marcel <marcel.sawicki@insert.com.pl>
2024-10-09 18:35:53 +0200

    cart

--- apm/src/app/cart/cart.service.ts
+++ apm/src/app/cart/cart.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from "@angular/core";
+import { effect, Injectable, signal } from "@angular/core";
+import { CartItem } from "./cart";
+import { Product } from "../products/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cartItems = signal<CartItem[]>([]);
 
+  eLength = effect(() => console.log('Cart array length:', this.cartItems().length));
+
+  addToCart(product: Product): void {
+    this.cartItems.update(items => [...items, { product, quantity: 1}])
+  }
 }
