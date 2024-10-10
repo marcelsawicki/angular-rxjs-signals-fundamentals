6ba3591116fdb3b5d93b407c70020c7cdf46ba48
Marcel <marcel.sawicki@insert.com.pl>
2024-10-10 11:24:51 +0200

    Quantity, Delete

--- apm/src/app/cart/cart.service.ts
+++ apm/src/app/cart/cart.service.ts
@@ -1,4 +1,4 @@
-import { effect, Injectable, signal } from "@angular/core";
+import { computed, effect, Injectable, signal } from "@angular/core";
 import { CartItem } from "./cart";
 import { Product } from "../products/product";
 
@@ -8,9 +8,31 @@ import { Product } from "../products/product";
 export class CartService {
   cartItems = signal<CartItem[]>([]);
 
+  cartCount = computed(() => this.cartItems()
+    .reduce((accQty, item) => accQty + item.quantity, 0));
+
+  subTotal = computed(() => this.cartItems().reduce((accTotal, item) =>
+    accTotal + (item.quantity * item.product.price),0));
+
+  deliveryFee = computed<number>(() => this.subTotal() < 50 ? 5.99 :0);
+  tax = computed(() => Math.round(this.subTotal() * 10.75)/100);
+
+  totalPrice = computed(() => this.subTotal() + this.deliveryFee() + this.tax());
+
   eLength = effect(() => console.log('Cart array length:', this.cartItems().length));
 
   addToCart(product: Product): void {
     this.cartItems.update(items => [...items, { product, quantity: 1}])
+  }
+
+  updateQuantity(cartItem: CartItem, quantity: number): void {
+    this.cartItems.update(items =>
+      items.map(item => item.product.id === cartItem.product.id ? { ...item, quantity } : item)
+    );
+  }
+
+  removeFromCart(cartItem: CartItem): void {
+    this.cartItems.update(items => 
+      items.filter(item => item.product.id !== cartItem.product.id));
   }
 }
